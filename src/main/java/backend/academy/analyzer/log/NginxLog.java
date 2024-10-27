17790e4e37b00c3cbbcc0817d85180fdd7362892
6ocra3 <6o3kmm@mail.ru>
2024-10-27 20:26:38 +0300

    feat: add converter to NgixnLog to Collector

--- src/main/java/backend/academy/analyzer/log/NginxLog.java
+++ src/main/java/backend/academy/analyzer/log/NginxLog.java
@@ -1,7 +1,9 @@
 package backend.academy.analyzer.log;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import lombok.Getter;
@@ -22,15 +24,17 @@ public class NginxLog {
         "^(\\S+) - - \\[(.+?)] \"(\\S+) (.+?) (\\S+)\" (\\d{3}) (\\d+) \"(.*?)\" \"(.*?)\"$"
     );
 
-    // Формат даты в строке лога
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z");
+    private static final DateTimeFormatter DATE_FORMATTER =
+        DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z", Locale.US);
 
     public NginxLog(String logLine) {
         Matcher matcher = LOG_PATTERN.matcher(logLine);
 
         if (matcher.find()) {
             this.ipAddress = matcher.group(1);
-            this.dateTime = LocalDateTime.parse(matcher.group(2), DATE_FORMATTER);
+
+            this.dateTime = OffsetDateTime.parse(matcher.group(2), DATE_FORMATTER).toLocalDateTime();
+
             this.httpMethod = HttpMethod.fromString(matcher.group(3));
             this.requestPath = matcher.group(4);
             this.httpVersion = matcher.group(5);
--- src/main/java/backend/academy/analyzer/statistic/StatisticCollector.java
+++ src/main/java/backend/academy/analyzer/statistic/StatisticCollector.java
@@ -1,21 +1,38 @@
 package backend.academy.analyzer.statistic;
 
+import backend.academy.analyzer.log.NginxLog;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class StatisticCollector {
 
-    public StatisticCollector(){};
+    public StatisticCollector() {
+    }
+
+    ;
 
     public void collectFromFile(String filePath) {
         Path path = Path.of(filePath);
         try (Stream<String> lines = Files.lines(path)) {
-            lines.forEach(System.out::println);
+            lines.map(line -> {
+                    try {
+                        return new NginxLog(line);
+                    } catch (IllegalArgumentException e) {
+                        System.err.println("Некорректная строка лога: " + line);
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .forEach(this::processLog);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private void processLog(NginxLog log) {
+        System.out.println(log.statusCode());
     }
 }
